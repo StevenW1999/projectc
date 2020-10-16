c0196a219cdb1e12a25ef069aadd1526b2a470b6
Ariano Wongsosetro <43781961+danchou-san@users.noreply.github.com>
2020-10-16 12:49:41 +0200

    Text fix

--- src/pages/productpage/ProductPage.js
+++ src/pages/productpage/ProductPage.js
@@ -37,7 +37,7 @@ class ProductPage extends Component {
                             <h1>Grote plant</h1>
                             <h4>Pieter</h4>
                             <br/>
-                            <p>Dit is mijn best plant ooit en ik ga dit nooit weggeven Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <p className="normal-text">Dit is mijn best plant ooit en ik ga dit nooit weggeven Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                             <Container className="text-center">
                                 <Button>Neem contact op</Button>
                             </Container>
