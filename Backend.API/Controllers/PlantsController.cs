381ac87967a964645f352ac10e8836e83213d241
StevenW1999 <0932549@hr.nl>
2020-11-17 16:40:44 +0100

    added git ignore for dotnet files

---
+++ Backend.API/Controllers/PlantsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectC.Data;
+using ProjectC.Models;
+
+namespace ProjectC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantsController : ControllerBase
+    {
+        private readonly ProjectCContext _context;
+
+        public PlantsController(ProjectCContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Plants
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Plant>>> GetPlants()
+        {
+            return await _context.Plants.ToListAsync();
+        }
+
+        // GET: api/Plants/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Plant>> GetPlant(int id)
+        {
+            var plant = await _context.Plants.FindAsync(id);
+
+            if (plant == null)
+            {
+                return NotFound();
+            }
+
+            return plant;
+        }
+
+        // PUT: api/Plants/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlant(int id, Plant plant)
+        {
+            if (id != plant.PlantId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(plant).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlantExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Plants
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Plant>> PostPlant(Plant plant)
+        {
+            _context.Plants.Add(plant);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PlantExists(plant.PlantId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetPlant", new { id = plant.PlantId }, plant);
+        }
+
+        // DELETE: api/Plants/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Plant>> DeletePlant(int id)
+        {
+            var plant = await _context.Plants.FindAsync(id);
+            if (plant == null)
+            {
+                return NotFound();
+            }
+
+            _context.Plants.Remove(plant);
+            await _context.SaveChangesAsync();
+
+            return plant;
+        }
+
+        private bool PlantExists(int id)
+        {
+            return _context.Plants.Any(e => e.PlantId == id);
+        }
+    }
+}
