00f181bd09ef4b991162c0177f91381ec3aecf42
Hatsikidee <60669911+Hatsikidee@users.noreply.github.com>
2020-11-21 17:32:04 +0100

    create link

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import {Layout} from './components/Layout';
 import About from './pages/about/About';
 import ProductPage from './pages/productpage/ProductPage';
+import Create_trade from './pages/create_trade/Create_trade';
 
 import {
   BrowserRouter as Router,
@@ -27,6 +28,7 @@ function App() {
           <Route exact path="/" component ={Home}/>
           <Route exact path="/About" component ={About}/>
           <Route exact path="/ProductPage" component ={ProductPage}/>
+          <Route exact path="/Createtrade" component ={Create_trade}/>
         </Switch>
       </Router>
       <Footer/>
--- client/src/pages/Create_trade/Create_trade.js
+++ client/src/pages/Create_trade/Create_trade.js
@@ -2,21 +2,12 @@ import React, { Component } from 'react';
 
 class Create_trade extends Component{
 render () {
-    return (
-      <div className="Create_trade">        
-              <Jumbotron fluid className="Jimbo">
-              <Container>
-                <h1>Hello plant</h1>
-            </Container>
-              </Jumbotron>
-                      <Container fluid>
-            <Row>
-              {plants}
-            </Row>
-          </Container>
-      </div>
-      );
-  }
+  return (
+    <div>  
+        <h1>Hello plant</h1>
+    </div>
+    );
+}
 }
 
 export default Create_trade;
