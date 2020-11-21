5b53a9bb76db443a51a8e8747f78175a28420a9b
Hatsikidee <60669911+Hatsikidee@users.noreply.github.com>
2020-11-21 16:58:48 +0100

    Update Create_trade.js

--- client/src/pages/Create_trade/Create_trade.js
+++ client/src/pages/Create_trade/Create_trade.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 
 class Create_trade extends Component{
 render () {
-  return (
-    <div>  
-        <h1></h1>
-    </div>
-    );
-}
+    return (
+      <div className="Create_trade">        
+              <Jumbotron fluid className="Jimbo">
+              <Container>
+                <h1>Hello plant</h1>
+            </Container>
+              </Jumbotron>
+                      <Container fluid>
+            <Row>
+              {plants}
+            </Row>
+          </Container>
+      </div>
+      );
+  }
 }
 
-export default [FileName];
+export default Create_trade;
