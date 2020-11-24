872c7c5da1382e4642c45b967f7c9f9948c97fd9
StevenW1999 <0932549@hr.nl>
2020-11-24 17:14:07 +0100

    wip refactoring code and about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/home/Home';
-import {Navigation} from './components/Navigation'
+import Navigation from './components/Navigation'
 import Footer from './components/Footer';
 import About from './pages/about/About';
 import ProductPage from './pages/productpage/ProductPage';
@@ -11,23 +11,18 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
-import { Layout } from './components/Layout';
 
 function App() {
   return (
-    <Layout>
-
-      <Router>
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component ={Home}/>
-          <Route exact path="/About" component ={About}/>
-          <Route exact path="/ProductPage" component ={ProductPage}/>
-        </Switch>
-      </Router>
-      <Footer/>
-
-    </Layout>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/ProductPage" component={ProductPage} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
