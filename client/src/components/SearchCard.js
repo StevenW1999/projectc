7787677a118bea52ea915945b0de624b593f1154
Kasper <vdpolkasper@gmail.com>
2020-11-26 17:13:44 +0100

    Start to the search results stuff
    had to start all over again due to retardation

---
+++ client/src/components/SearchCard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+class SearchCard extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div style={{ "border-style": "solid" }, { "overflow": "hidden" }}>
+				<Card style={{ width: '18rem' }}>
+					<Card.Img variant="top" src="holder.js/100px180" />
+					<Card.Body>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Text>
+					    </Card.Text>
+						<Button variant="primary">Plant bekijken</Button>
+					</Card.Body>
+				</Card>
+			</div>
+		)
+	}
+}
+
+export default SearchCard
