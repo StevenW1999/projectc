af06207cfb1afaaf453cd66cedee9804c931d67f
Hatsikidee <60669911+Hatsikidee@users.noreply.github.com>
2020-11-27 12:12:57 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import {Navigation} from './components/Navigation'
 import Footer from './components/Footer';
 import About from './pages/about/About';
 import ProductPage from './pages/productpage/ProductPage';
-import Create_trade from './pages/create_trade/Create_trade';
+import Create_trade from './pages/Create_trade/Create_trade';
 
 import {
   BrowserRouter as Router,
@@ -24,7 +24,7 @@ function App() {
           <Route exact path="/" component ={Home}/>
           <Route exact path="/About" component ={About}/>
           <Route exact path="/ProductPage" component ={ProductPage}/>
-          <Route exact path="/Createtrade" component ={Create_trade}/>
+          <Route exact path="/Create_trade" component ={Create_trade}/>
         </Switch>
       </Router>
       <Footer/>
