dc45b68b33fe646d99f1a57d0f1b19d4a2cc8aa4
Ariano <arianowong@gmail.com>
2020-11-27 17:35:39 +0100

    Update productpage

--- client/src/pages/productpage/ProductPage.js
+++ client/src/pages/productpage/ProductPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './ProductPage.css';
 import {Carousel, Container, Col, Row, Button, Card} from 'react-bootstrap';
+import {BsGeoAlt, BsDroplet, BsBrightnessHigh, BsStar} from 'react-icons/bs';
 import styled from 'styled-components';
 // import PlantItem from '../../components/PlantItem';
 import { render } from '@testing-library/react';
@@ -8,10 +9,20 @@ import { render } from '@testing-library/react';
 class ProductPage extends Component {
     render() {
         return (
-            <Container>
+            <Container className="container-padding">
                 <Row>
                     <Col md={6}>
                         <Carousel className="gbackground">
+                            <Carousel.Item>
+                                <img
+                                    className="image d-block fit-gallery"
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="image d-block fit-gallery"
+                                />
+                            </Carousel.Item>
                             <Carousel.Item>
                                 <img
                                     className="image d-block fit-gallery"
@@ -33,10 +44,17 @@ class ProductPage extends Component {
                         <Container className="dbackground">
                             <h1>Grote plant</h1>
                             <h4>Pieter</h4>
+                            <p className="normal-text">1 maand actief op Bio Diversity</p>
+                            <a href="">Bekijk meer planten van Pieter</a>
                             <br/>
-                            <p className="normal-text">Dit is mijn best plant ooit en ik ga dit nooit weggeven Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <br/>
+                            <p className="normal-text"><BsGeoAlt/> Vlaardingen</p>
+                            <br/>
+                            <p className="normal-text">Plant details:</p>
+                            <p className="normal-text font-weight-bold"><BsDroplet/> Om de 2 weken water geven</p>
+                            <p className="normal-text font-weight-bold"><BsBrightnessHigh/> Heeft weinig zon nodig</p>
                             <Container className="text-center">
-                                <Button>Neem contact op</Button>
+                                <Button>Neem contact op</Button> <BsStar/>
                             </Container>
                             <br/>
                         </Container>
@@ -44,6 +62,13 @@ class ProductPage extends Component {
                     </Col>
                 </Row>
 
+                <br/>
+
+                <Container className="dbackground">
+                    <h2>Beschrijving</h2>
+                    <p className="normal-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elementum sem in magna sollicitudin volutpat. Sed lorem ante, volutpat et leo in, mattis tincidunt elit. In ultricies et turpis sed mattis. Nullam non metus bibendum, molestie lectus sit amet, lobortis ipsum. Nullam id consequat leo. Donec aliquet iaculis nunc, eu auctor odio dictum eget. Duis venenatis at lectus id tristique. Phasellus porttitor nulla sed porttitor ultricies. Sed ullamcorper semper eleifend. Vivamus vulputate erat dolor, ut rutrum libero euismod at. Mauris convallis turpis quis mi commodo bibendum.</p>
+                    <br/>
+                </Container>
                 <br/>
 
                 <Row>
@@ -78,6 +103,10 @@ class ProductPage extends Component {
                         </div>
                     </Col>
                 </Row>
+
+                <Container className="text-center">
+                    <Button>Meer laden...</Button>
+                </Container>        
 
                 <br/>
             </Container>
