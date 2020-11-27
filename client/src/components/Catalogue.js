3c7613f50231df699583910185f23e95cef029b2
Kasper <vdpolkasper@gmail.com>
2020-11-27 19:22:13 +0100

    Search results pagina overhaul. Categorieën aan de cataloge toegevoegd

--- client/src/components/Catalogue.js
+++ client/src/components/Catalogue.js
@@ -15,6 +15,10 @@ class Catalogue extends Component {
                 <PlantItem plant={plant} />
             )
         })
+
+   
+                                // TO-DO: VERANDER 'as="select"'
+
         return (
             <>
                 <div class="row">
@@ -26,18 +30,66 @@ class Catalogue extends Component {
                                 <Form.Control type="text" placeholder="SEARCH" />
                                 <br />
                                 <Form.Control as="select">
-                                    <option>categories</option>
+                                    <option>Type plant...</option>
+                                    <option>Boom</option>
+                                    <option>Struik</option>
+                                    <option>Kruidachtige</option>
+                                    <option>Bodembedekker</option>
+                                    <option>klimplant</option>
+                                    <option>Vijverplant</option>
                                 </Form.Control>
                                 <br />
                                 <Form.Control as="select">
-                                    <option>types</option>
+                                    <option>Vaste plant...</option>
+                                    <option>Ja</option>
+                                    <option>Nee</option>
                                 </Form.Control>
                                 <br />
-                                <Form.Group controlId="formBasicRange">
-                                    <Form.Label>km range</Form.Label>
-                                    <Form.Control type="range" />
-                                </Form.Group>
-
+                                <Form.Control as="select">
+                                    <option>Standplaats...</option>
+                                    <option>Zon</option>
+                                    <option>Halfschaduw</option>
+                                    <option>Schaduw</option>
+                                </Form.Control>
+                                <br />
+                                <Form.Control as="select">
+                                    <option>Water...</option>
+                                    <option>Nat</option>
+                                    <option>Gemiddeld</option>
+                                    <option>Droog</option>
+                                </Form.Control>
+                                <br />
+                                <Form.Control as="select">
+                                    <option>Groeihoogte</option>
+                                    <option>0-20 cm</option>
+                                    <option>20-40 cm</option>
+                                    <option>40-70 cm</option>
+                                    <option>70-100 cm</option>
+                                    <option>100-120 cm</option>
+                                    <option>120-150 cm</option>
+                                    <option>1,5 - 2 m</option>
+                                    <option>Hoger dan 2m</option>
+                                </Form.Control>
+                                <br />
+                                <Form.Control as="select">
+                                    <option>Kleur...</option>
+                                    <option>Blauw</option>
+                                    <option>Geel</option>
+                                    <option>Rood</option>
+                                    <option>Violetblauw</option>
+                                    <option>Wit</option>
+                                    <option>Zwart</option>
+                                    <option>Anders</option>
+                                </Form.Control>
+                                <br />
+                                <Form.Control as="select">
+                                    <option>Afstand...</option>
+                                    <option>&lt;5KM</option>
+                                    <option>&lt;15KM</option>
+                                    <option>&lt;50KM</option>
+                                    <option>&lt;100KM</option>
+                                </Form.Control>
+                                <br />
                             </Form>
                         </div>
                     </div>
--- client/src/pages/searchresults/Searchresults.js
+++ client/src/pages/searchresults/Searchresults.js
@@ -3,32 +3,65 @@ import './Searchresults.css';
 import SearchResult from '../../components/SearchResult';
 import PlantItem from '../../components/PlantItem';
 import SearchCard from '../../components/SearchCard';
+import Catalogue from '../../components/Catalogue';
 
 class Searchresults extends Component{
     constructor(){
 	super();
-	this.state = {
-	    results: [
-		{
-		    id: 1,
-		    name: 'Psilocybe Cubensis',
-		    image: './images/BG2.jpg',
-		    description: 'De Psilocybe cubensis is een soort van de psychedelische paddenstoelen, dat betekent dat de aanwezige stoffen euforie, visuele hallucinaties en algemene veranderingen in perceptie veroorzaken.' 
-		},
-		{
-		    id: 2,
-		    name: 'Hawaiian Baby Woodrose',
-		    image: './images/BG2.jpg',
-		    description: 'lorem memes'
-		},
-		{
-		    id: 3,
-		    name: 'result2',
-		    image: './images/BG2.jpg',
-		    description: 'lorem memes'
-		}
+        this.state = {
+			results: [
+				{
+					id: 1,
+					name: 'Psilocybe Cubensis',
+					image: './images/BG2.jpg',
+					description: 'De Psilocybe cubensis is een soort van de psychedelische paddenstoelen, dat betekent dat de aanwezige stoffen euforie, visuele hallucinaties en algemene veranderingen in perceptie veroorzaken.'
+				},
+				{
+					id: 2,
+					name: 'Hawaiian Baby Woodrose',
+					image: './images/BG2.jpg',
+					description: 'lorem memes'
+				},
+				{
+					id: 3,
+					name: 'result2',
+					image: './images/BG2.jpg',
+					description: 'lorem memes'
+				}
 
-	    ]
+			],
+        plants: [{
+
+            id: 0,
+            name: 'plant1',
+            image: '../../images/BG2.jpg',
+            description: 'Lorem Ipsum Dolor etc blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla'
+        },
+        {
+            id: 1,
+            name: 'plant2',
+            image: '../../images/BG2.jpg',
+            description: 'Lorem Ipsum Dolor etc blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla'
+        },
+        {
+            id: 2,
+            name: 'plant3',
+            image: '../../images/BG2.jpg',
+            description: 'Lorem Ipsum Dolor etc blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla'
+        },
+        {
+            id: 3,
+            name: 'plant4',
+            image: '../../images/BG2.jpg',
+            description: 'Lorem Ipsum Dolor etc blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla'
+        },
+        {
+            id: 4,
+            name: 'plant5',
+            image: '../../images/BG2.jpg',
+            description: 'Lorem Ipsum Dolor etc blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla'
+        },
+        ]
 	}
     }
 
@@ -40,57 +73,12 @@ class Searchresults extends Component{
 		})
 
 		return (
-			<div className="Center">
-				<div className="TopBar">
-					<div className="TopBarBox">
-						<input type="text" placeholder="Zoek..." class="btn-sm" >
-						</input>
-						<select name="categories" style={{ width: '20%', margin: '17px', padding: '2px' }} className="categoriesList">
-								<option value="option 1">Option 1</option>
-								<option value="option 1">Option 2</option>
-								<option value="option 1">Option 3</option>
-							</select>
-						<input type="text" placeholder="Postcode..." style={{ width: '12%' }}></input>
-
-						<button type="button" class="btn btn-primary btn-sm">Zoek!</button>
-						</div>
+            <>
+                <div className="cata">
                     </div>
-				<div className="BelowCenter">
-                    <br/>
-                    <br />
-					<div className="ParentDiv">
-						<div className="CategoriesParent">
-							<select name="categories"  className="categoriesList">
-								<option value="option 1">Option 1</option>
-								<option value="option 1">Option 2</option>
-								<option value="option 1">Option 3</option>
-							</select>
-							<div className="categoriesTickboxes">
-								<div className="TickboxesPadding">
-                                    <input type="checkbox" id="Grote plant" name="Grote plant" value="Bike"/>
-                                    <label for="Grote plant">Grote plant</label>
-                                    <br/>
-                                    <input type="checkbox" id="Kleine plant" name="Kleine plant" value="Bike"/>
-                                    <label for="Kleine plant">Kleine plant</label>
-                                    <br/>
-                                    <input type="checkbox" id="Kleine schaduw" name="Kleine schaduw" value="Bike"/>
-                                        <label for="Kleine schaduw">Kleine schaduw</label>
-									</div>
-								</div>
-						</div>
-						<div className="ItemsContainer">
-							<div class="col-md-4">
-								<SearchCard className="SearchCards" />
-							</div>
-							<div class="col-md-8">
-								<SearchCard className="SearchCards" />
-							</div>
-						</div>
-						</div>
-							<div className="bottomButtonsContainer">
-					</div>
-				</div>
-				</div>
+                    <Catalogue data={this.state.plants}></Catalogue>
+                    <div className="cata"></div>
+				</>
         );
     }
 }
