eba1ba91f8a04eb12908b993280587a09604aef2
TheLoneWolf13 <47594026+TheLoneWolf13@users.noreply.github.com>
2020-11-27 17:51:18 +0100

    Revert "Revert "master to accountpage""
    This reverts commit a5fdae400df62015576ad9faf3866441ec9540da.

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,36 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/home/Home';
-import {Navigation} from './components/Navigation'
+import Navigation from './components/Navigation'
 import Footer from './components/Footer';
 import About from './pages/about/About';
 import ProductPage from './pages/productpage/ProductPage';
+import Searchresults from './pages/searchresults/Searchresults';
 import AccountCreate from './pages/accountCreate/AccountCreate';
 import Account from './pages/account/Account';
+
 
 import {
   BrowserRouter as Router,
   Route,
   Switch,
 } from "react-router-dom";
-import { Layout } from './components/Layout';
 
 function App() {
   return (
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/ProductPage" component={ProductPage} />
+        <Route exact path="/Search" component={Searchresults} />
+        <Route exact path="/Login" component ={AccountCreate}/>
+        <Route exact path="/Account" component ={Account}/>
+      </Switch>
+      <Footer />
+    </Router>
     <Layout>
-
-      <Router>
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component ={Home}/>
-          <Route exact path="/About" component ={About}/>
-          <Route exact path="/ProductPage" component ={ProductPage}/>
-          <Route exact path="/Login" component ={AccountCreate}/>
-          <Route exact path="/Account" component ={Account}/>
-        </Switch>
-      </Router>
-      <Footer/>
-
-    </Layout>
   );
 }
 
