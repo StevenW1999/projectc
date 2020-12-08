0ab4c486d6569b338ee5bbc074f676625a9bc4a3
StevenW1999 <0932549@hr.nl>
2020-12-08 20:18:23 +0100

    fixed migration bullshit bug

--- Migrations/20201208185531_UpdatePLants.cs
+++ Migrations/20201208185531_UpdatePLants.cs
@@ -6,12 +6,27 @@ namespace ProjectC.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+         migrationBuilder.AlterColumn<string>(
+         name: "GrowthHeigth",
+         table: "Plants",
+         type: "text",
+         nullable: true,
+         oldClrType: typeof(int),
+         oldType: "integer");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AlterColumn<int>(
+            name: "GrowthHeigth",
+            table: "Plants",
+            type: "integer",
+            nullable: false,
+            defaultValue: false,
+            oldClrType: typeof(string),
+            oldType: "text",
+            oldNullable: true);
         }
     }
 }
+
