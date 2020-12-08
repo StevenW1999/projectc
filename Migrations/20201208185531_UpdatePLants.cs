370a7ef2a9d534374acb259c03822499099f6011
StevenW1999 <0932549@hr.nl>
2020-12-08 19:56:21 +0100

    added migration

---
+++ Migrations/20201208185531_UpdatePLants.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectC.Migrations
+{
+    public partial class UpdatePLants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
