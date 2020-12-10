bec9079894861c1d16bbe0b1994578a4d9aa97a0
Kasper <vdpolkasper@gmail.com>
2020-12-10 15:49:40 +0100

    begin van het zoeksysteem adminpanel

--- ClientApp/src/pages/adminpanel/AdminPanel.js
+++ ClientApp/src/pages/adminpanel/AdminPanel.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import User from '../../components/User';
 import './AdminPanel.css';
 import UserCatalogue from '../../components/UserCatalogue';
+import Form from 'react-bootstrap/Form';
 
 class AdminPanel extends Component {
     constructor() {
@@ -20,14 +21,74 @@ class AdminPanel extends Component {
                     image: 'images/Default-User.jpg',
                     description: 'Ik ben Tim en ik ben slim'
                 }
-            ]
+            ],
+            userList: null,
+            zoek: ""
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        event.preventDefault();
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+        this.preventDefaultFilters();
+    }
+
+    preventDefaultFilters() {
+        if (this.state.zoek == "Gebruikersnaam...") {
+            this.state.zoek = "";
         }
     }
+
+    componentDidMount() {
+        fetch('https://localhost:44338/api/Users', {
+            method: 'get',
+            headers: {
+                'Authorization': 'bearer ' + localStorage.getItem('bearer')
+            }
+        })
+            .then(response => response.json())
+            .then(data => this.setState({
+                userList: data            }))    }
+
+    getData() {
+        if (this.state.userList === null) {
+            return [{
+                id: 0,
+                name: 'waiting',
+                image: './',
+                description: 'waiting'
+            }]
+        }
+        return this.state.userList;
+    }
+
 
     render() {
         return (
             <>
-                <UserCatalogue data={this.state.users} />
+                <br/>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-">
+                            <Form>
+                                <div class="input-group mb-3">
+                                <Form.Control type="text" placeholder="Gebruikersnaam..."/>
+                                    <div class="input-group-append">
+                                        <button type="button" class="btn btn-success">Zoek!</button>
+                                    </div>
+                                </div>
+                    </Form>
+                    </div>
+                    </div>
+                    </div>
+                <UserCatalogue data={this.getData()} />
                 </>
         )
     }
