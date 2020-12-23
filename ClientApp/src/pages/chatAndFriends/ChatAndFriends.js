c02dc4484d0ae1525d6eb68062fd76d1c4dd0ed7
TheLoneWolf13 <47594026+TheLoneWolf13@users.noreply.github.com>
2020-12-23 12:10:03 +0100

    Basic setup (no packages yet!)

---
+++ ClientApp/src/pages/chatAndFriends/ChatAndFriends.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+class ChatAndFriends extends Component{
+    render() {
+        return (
+            <div>
+                <h1></h1>
+            </div>
+        );
+    }
+}
+
+export default ChatAndFriends;
