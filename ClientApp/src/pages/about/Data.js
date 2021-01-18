c06e1e124fd2c961a2c8ab1ee27b860959db010f
Kasper <vdpolkasper@gmail.com>
2021-01-18 15:11:46 +0100

    stekhub > stekoverflow

--- ClientApp/src/pages/about/Data.js
+++ ClientApp/src/pages/about/Data.js
@@ -6,8 +6,8 @@ export const aboutObjOne = {
     headline: 'STADSKWEKERIJ DE KAS',
     description:
         'Stadskwekerij De Kas is een bewonersinitiatief van Eva Bes en Marja Versteeg, twee tuinvrouwen uit Rotterdam Noord met hart voor de natuur en mens. Samen met al onze vrijwilligers, zetten we ons in voor de biodiversiteit in de stad en voor de verbinding tussen mensen en organisaties.Thuisbasis van Stadskwekerij De Kas is een kassencomplex in het hartje van Rotterdam. Daar hebben we een duurzame kwekerij met inspiratietuin.',
-    description2: 'In 2020 groeide de vraag naar onze gifvrije plantjes. Het frustreerde ons dat we door ruimte- en tijdgebrek niet konden voldoen aan deze vraag. Tegelijkertijd boden steeds meer bewoners ons spontaan hun zaailingen en stekjes aan. Toen ontstond al snel het idee een digitaal platform te ontwikkelen waar we vraag en aanbod bij elkaar kunnen komen. Studenten van de Hogeschool Rotterdam schoten ons te hulp en zo onstond Stekoverflow.',
-    description3: 'Stekoverflow biedt een platform waarop alle groenliefhebbers planten en kennis met elkaar kunnen delen.Zodat we samen een grote duurzame kwekerij vormen die zich inzet voor een gifvrije, betaalbare en groene wereld.',
+    description2: 'In 2020 groeide de vraag naar onze gifvrije plantjes. Het frustreerde ons dat we door ruimte- en tijdgebrek niet konden voldoen aan deze vraag. Tegelijkertijd boden steeds meer bewoners ons spontaan hun zaailingen en stekjes aan. Toen ontstond al snel het idee een digitaal platform te ontwikkelen waar we vraag en aanbod bij elkaar kunnen komen. Studenten van de Hogeschool Rotterdam schoten ons te hulp en zo onstond Stekhub.',
+    description3: 'Stekhub biedt een platform waarop alle groenliefhebbers planten en kennis met elkaar kunnen delen.Zodat we samen een grote duurzame kwekerij vormen die zich inzet voor een gifvrije, betaalbare en groene wereld.',
     imgStart: '',
     img: 'images/plant1.png',
     alt: 'plant1'
@@ -20,7 +20,7 @@ export const aboutObjOne = {
     topLine: 'ONS DOEL',
     headline: 'BIO DIVERSITY',
     description:
-      'Meer groen is goed voor bijen, vlinders en andere dieren. Maar dan moet dat wel groen zijn, zonder gif! In tuincentra en supermarkten zijn gifvrije planten moeilijk verkrijgbaar en prijzig. Stekoverflow biedt groenliefhebbers een platform om goedkoop planten, zaden en kennis met elkaar te delen, zonder gif!',
+      'Meer groen is goed voor bijen, vlinders en andere dieren. Maar dan moet dat wel groen zijn, zonder gif! In tuincentra en supermarkten zijn gifvrije planten moeilijk verkrijgbaar en prijzig. Stekhub biedt groenliefhebbers een platform om goedkoop planten, zaden en kennis met elkaar te delen, zonder gif!',
     imgStart: '',
     img: 'images/biodiversity.jpg',
     alt: 'biodiversity'
@@ -32,11 +32,11 @@ export const aboutObjOne = {
     lightTextDesc: false,
     topLine: 'HOE WERKT HET?',
     headline: 'Super natuurvriendelijk en simpel ruilen',
-    description: '1. Op Stekoverflow worden alleen tuinplanten aangeboden.Het gaat ons immers om de biodiversiteit.',
+    description: '1. Op Stekhub worden alleen tuinplanten aangeboden.Het gaat ons immers om de biodiversiteit.',
     description2: '2. Het plantmateriaal dat u aanbiedt, is (voor zover u weet) gekweekt zonder pesticiden of andere gifstoffen.',
-    description3: '3. Stekoverflow is een platform om te ruilen en//of te delen met elkaar. Het platform is niet bedoeld voor commerciele doeleinden. Er wordt dus niet gehandeld met geld.',
-    description4: '4. Op Stekoverflow mag je geen planten aanbieden die kweekrechtelijk beschermd zijn. Een plant is kweekrechtelijk beschermd als e r PBR of PFAFF achter de plantnaam staat. Kijk hiervoor op: https://www.plantago.nl/kwekersrecht.html.',
-    description5: '5. Stekoverflow is niet verantwoordelijk voor de kwaliteit van het aanbod.',
+    description3: '3. Stekhub is een platform om te ruilen en//of te delen met elkaar. Het platform is niet bedoeld voor commerciele doeleinden. Er wordt dus niet gehandeld met geld.',
+    description4: '4. Op Stekhub mag je geen planten aanbieden die kweekrechtelijk beschermd zijn. Een plant is kweekrechtelijk beschermd als e r PBR of PFAFF achter de plantnaam staat. Kijk hiervoor op: https://www.plantago.nl/kwekersrecht.html.',
+    description5: '5. Stekhub is niet verantwoordelijk voor de kwaliteit van het aanbod.',
     description6: '6. We willen graag stimuleren dat het delen en ruilen zo duurzaam mogelijk gebeurt. Daarom zie je bij ieder aanbod direct op welke afstand jet het kunt vinden. We zouden het leuk vinden als je de planten zoveel mogelijk lopend, fietsend of met het OV gaat halen.',
     imgStart: 'start',
     img: 'images/seedling.svg',
