95d958310af6e2ce7c734379f9b4c04f825a1d5d
StevenW1999 <0932549@hr.nl>
2021-01-22 19:25:00 +0100

    Revert "Revert "Merge branch 'backend-chat-Steven'""
    This reverts commit bdefb8c8448a4d5ed752c29b0c14f6a5e9c49f60.

---
+++ Models/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class ChatMessage
+    {
+        public Guid Id { get; set; }
+        public Guid RoomId { get; set; }
+        [Required]
+        public string Contents { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        public DateTimeOffset PostedAt { get; set; }
+    }
+}
---
+++ Services/ChatService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Services
+{
+    public interface IChatService
+    {
+        Task<List<ChatRoom>> GetChatRoomsAsync();
+        Task<bool> AddChatRoomAsync(ChatRoom newChatRoom);
+        public bool DoesChatRoomExist(string name);
+    }
+    public class ChatService : IChatService
+    {
+        private readonly ProjectCContext _context;
+
+        public ChatService(ProjectCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ChatRoom>> GetChatRoomsAsync()
+        {
+            var chatRooms = await _context.ChatRooms.ToListAsync<ChatRoom>();
+
+            return chatRooms;
+        }
+
+        public async Task<bool> AddChatRoomAsync(ChatRoom chatRoom)
+        {
+            if (!DoesChatRoomExist(chatRoom.Name))
+            {
+                chatRoom.Id = Guid.NewGuid();
+
+                _context.ChatRooms.Add(chatRoom);
+
+                var saveResults = await _context.SaveChangesAsync();
+
+                return saveResults > 0;
+            }
+            return false;
+        }
+        public bool DoesChatRoomExist(string name)
+        {
+            var chatrooms = _context.ChatRooms.FirstOrDefault(c => c.Name == name);
+            if(chatrooms == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
